88ff12191c6ce1337158624f63dd3e38fe5c0e70
shamtupe2014 <49332026+shamtupe2014@users.noreply.github.com>
2024-03-25 14:24:46 +0530

    Update WorksWithHerokuServletTest.java

--- src/test/java/works/buddy/samples/WorksWithHerokuServletTest.java
+++ src/test/java/works/buddy/samples/WorksWithHerokuServletTest.java
@@ -36,6 +36,6 @@ public class WorksWithHerokuServletTest {
         when(response.getWriter()).thenReturn(writer);
 
         servlet.doGet(request, response);
-        assertEquals("Buddy Works with NBT", new String( out.toByteArray(), "UTF-8"));
+        assertEquals("Buddy Works with NBT-updated", new String( out.toByteArray(), "UTF-8"));
     }
 }
